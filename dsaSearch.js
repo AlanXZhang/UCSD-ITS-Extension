7af42a0ed6484005a7fb64371a86278518ecded7
acmstechnicians <acmstechnicians@AD.UCSD.EDU>
2022-08-12 13:14:55 -0700

    fixed the dsa search tool to lowercase the email and trim the input

--- dsaSearch.js
+++ dsaSearch.js
@@ -22,20 +22,21 @@ function setupButtons() {
 }
 
 const currURL = document.URL
-
+console.log(currURL)
+console.log(currURL.indexOf("https://iam.ucsd.edu/dsasearch/dsaSearchByEmail*") > -1)
 if (currURL.indexOf("https://iam.ucsd.edu/dsasearch/deptList") > -1) {
   var tableParent = document.getElementById("results");
   const observer = new MutationObserver(() => {setupButtons()});
   observer.observe(tableParent, {childList: true});
-} else { //currURL.indexOf("https://iam.ucsd.edu/dsasearch/dsaSearchByEmail*") > -1
+} else {
   setupButtons()
   var inputForm = document.querySelector("input");
   console.log(inputForm)
   if (inputForm) {
     inputForm.addEventListener('input', (e) => {
       var email = e.target.value
-      console.log(email.toLowerCase())
-      inputForm.value = email.toLowerCase()
+      console.log(email.toLowerCase().trim())
+      inputForm.value = email.toLowerCase().trim()
       navigator.clipboard.writeText(`${email}`);
     });
   }
