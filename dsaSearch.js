f60d679ea7f642cdf6f674141cc158334de7db08
acmstechnicians <acmstechnicians@AD.UCSD.EDU>
2022-08-12 12:46:30 -0700

    pushing v1

---
+++ dsaSearch.js
@@ -0,0 +1,42 @@
+var iconURL = chrome.runtime.getURL('copy.png');
+
+function setupButtons() {
+  var tables = document.querySelectorAll('table');
+
+  for (var table of tables) {
+    var button = document.createElement('button');
+    button.setAttribute("id", table.id);
+
+    button.onclick = function(e) {
+      var clipboardString = e.target.parentElement.outerHTML;
+      clipboardString = clipboardString.replace(/ {4}|[\t\n\r]/gm,'');
+      navigator.clipboard.writeText(`[code]${clipboardString}[/code]`);
+    };
+
+    button.setAttribute("style","display:none")
+    button.setAttribute("class","table-copy");
+
+    table.style.setProperty('--url', `url(${iconURL})`);
+    table.appendChild(button);
+  }
+}
+
+const currURL = document.URL
+
+if (currURL.indexOf("https://iam.ucsd.edu/dsasearch/deptList") > -1) {
+  var tableParent = document.getElementById("results");
+  const observer = new MutationObserver(() => {setupButtons()});
+  observer.observe(tableParent, {childList: true});
+} else { //currURL.indexOf("https://iam.ucsd.edu/dsasearch/dsaSearchByEmail*") > -1
+  setupButtons()
+  var inputForm = document.querySelector("input");
+  console.log(inputForm)
+  if (inputForm) {
+    inputForm.addEventListener('input', (e) => {
+      var email = e.target.value
+      console.log(email.toLowerCase())
+      inputForm.value = email.toLowerCase()
+      navigator.clipboard.writeText(`${email}`);
+    });
+  }
+}
