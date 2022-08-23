b4d1e124e827f48e32cf6fcd87cd6ae9e442d0c7
Kevin Fisher <kfish610@gmail.com>
2022-08-23 05:12:41 +0000

    Remove extraneous permission

--- snowListener.js
+++ snowListener.js
@@ -1,57 +1,61 @@
 function editHighlighted(style) {
-  var doc = document.getElementById("gsft_main")?.contentWindow?.document ?? document;
+    var doc =
+        document.getElementById("gsft_main")?.contentWindow?.document ??
+        document;
 
-  var selection = doc.getSelection();
-  if (selection.focusNode !== selection.anchorNode) return;
+    var selection = doc.getSelection();
+    if (selection.focusNode !== selection.anchorNode) return;
 
-  var textArea = selection.focusNode.querySelector("textarea");
+    var textArea = selection.focusNode.querySelector("textarea");
 
-  var len = textArea.value.length;
-  var start = textArea.selectionStart;
-  var end = textArea.selectionEnd;
+    var len = textArea.value.length;
+    var start = textArea.selectionStart;
+    var end = textArea.selectionEnd;
 
-  var startCode, endCode, selectionEnd;
+    var startCode, endCode, selectionEnd;
 
-  if (style === "hyperlink") {
-    startCode = '[code]<a href="">';
-    endCode = "</a>[/code] ";
-    selectionEnd = start + startCode.indexOf('"') + 1;
-  } else if (style === "bold") {
-    startCode = "[code]<b>";
-    endCode = "</b>[/code] ";
-    selectionEnd =
-      end - start > 0
-        ? end + startCode.length + endCode.length - 1
-        : start + startCode.length;
-  } else if (style === "image") {
-    startCode = `[code]<img src="`;
-    endCode = `" width=600px />[/code] `;
-    selectionEnd =
-      end - start > 0
-        ? end + startCode.length + endCode.length - 1
-        : start + startCode.length;
-  }
+    if (style === "hyperlink") {
+        startCode = '[code]<a href="">';
+        endCode = "</a>[/code] ";
+        selectionEnd = start + startCode.indexOf('"') + 1;
+    } else if (style === "bold") {
+        startCode = "[code]<b>";
+        endCode = "</b>[/code] ";
+        selectionEnd =
+            end - start > 0
+                ? end + startCode.length + endCode.length - 1
+                : start + startCode.length;
+    } else if (style === "image") {
+        startCode = `[code]<img src="`;
+        endCode = `" width=600px />[/code] `;
+        selectionEnd =
+            end - start > 0
+                ? end + startCode.length + endCode.length - 1
+                : start + startCode.length;
+    }
 
-  textArea.value =
-    textArea.value.substring(0, start) +
-    startCode +
-    textArea.value.substring(start, end).trim() +
-    endCode +
-    textArea.value.substring(end, len);
-  textArea.selectionEnd = selectionEnd;
+    textArea.value =
+        textArea.value.substring(0, start) +
+        startCode +
+        textArea.value.substring(start, end).trim() +
+        endCode +
+        textArea.value.substring(end, len);
+    textArea.selectionEnd = selectionEnd;
 }
 
 chrome.commands.onCommand.addListener((command, tab) => {
-  if (tab === undefined || (
-    !tab.url.startsWith("https://support.ucsd.edu/")
-    && !tab.url.startsWith("https://snqa.ucsd.edu/")
-  )) {
-    return;
-  }
+    if (
+        tab === undefined ||
+        tab.url === undefined ||
+        (!tab.url.startsWith("https://support.ucsd.edu/") &&
+            !tab.url.startsWith("https://snqa.ucsd.edu/"))
+    ) {
+        return;
+    }
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: editHighlighted,
-    args: [command]
-  });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: editHighlighted,
+        args: [command],
+    });
 });
