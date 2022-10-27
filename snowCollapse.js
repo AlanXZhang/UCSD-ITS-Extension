8030d9e28950e01f9ae6f481ac78ac1f0f5a228e
Ari Stassinopoulos <stassinopoulosari@gmail.com>
2022-10-27 11:19:57 -0700

    Rework some parts of Snow Collapse

--- snowCollapse.js
+++ snowCollapse.js
@@ -3,35 +3,10 @@
     document.getElementById("gsft_main")?.contentWindow?.document ??
     document;
 
-  if (doc !== document) {
-    var observer = new MutationObserver((mutationList) => {
-      var doc =
-        document.getElementById("gsft_main")?.contentWindow?.document ??
-        document;
-      var textBlocks = [].slice.call(doc.querySelectorAll('span.sn-widget-textblock-body.sn-widget-textblock-body_formatted'))
-      textBlocks.forEach((block) => {
-        var lines = block.innerHTML.split("<br>"),
-          collections = [],
-          i = 0
-        lines.forEach((line) => {
-          if (line.trim().slice(0, 4) == '&gt;') {
-            if (collections[i] == undefined) {
-              collections[i] = []
-            }
-            collections[i].push(line);
-          } else {
-            i += 1;
-            collections[i] = [];
-            collections[i].push(line)
-          }
-        });
-        block.innerHTML = collections.map(
-          (collection) => collection.length == 1 ? collection[0] :
-          "<details><summary>" + collection[0] + "</summary>" +
-          "<p>" + collection.slice(1).join("</p><p>") + "</p></details>").join('<br>')
-      })
-    });
-  } else {
+  var snowCollapse = () => {
+    var doc =
+      document.getElementById("gsft_main")?.contentWindow?.document ??
+      document;
     var textBlocks = [].slice.call(doc.querySelectorAll('span.sn-widget-textblock-body.sn-widget-textblock-body_formatted'))
     textBlocks.forEach((block) => {
       var lines = block.innerHTML.split("<br>"),
@@ -51,9 +26,20 @@
       });
       block.innerHTML = collections.map(
         (collection) => collection.length == 1 ? collection[0] :
-        "<details><summary>" + collection[0] + "</summary>" +
-        "<p>" + collection.slice(1).join("</p><p>") + "</p></details>").join('<br>')
+        "<details><summary>Collapsed Quote</summary>" +
+        "<p>" + collection.join("</p><p>") + "</p></details>").join('<br>')
     })
+  }
+
+  if (doc !== document) {
+    var observer = new MutationObserver((mutationList) => {
+      snowCollapse();
+    });
+    observer.observe(document.getElementById('gsft_main'), {
+      attributes: true
+    })
+  } else {
+    snowCollapse();
   }
 
 
